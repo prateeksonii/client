a8fae73c9409132af18ee967f6a30e9df2234397
Prateek <prateek_soni@outlook.com>
2022-02-10 16:20:37 +0530

    initialize app with tailwind and vite 🆕

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        body: ["Human Sans", "sans-serif"],
+        head: ["Gilroy", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
